b739be1576994f5fc6b806d5aa26cc1b37fd3188
golam71 <megenius408@gmail.com>
2023-11-10 20:54:06 +0600

    :zap: blog markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress"; // https://astro.build/config
 import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://salam.app",
-  integrations: [tailwind(),  solidJs(), compress()]
+  integrations: [tailwind(), mdx(), solidJs(), compress()]
 
 
 });
