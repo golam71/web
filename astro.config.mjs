091997b4608f5d98620a522e09b11a655f97dcc6
golam71 <mrgenius408@gmail.com>
2023-11-18 20:46:41 +0600

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
+import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress"; // https://astro.build/config
+//import mdx from "@astrojs/mdx"
+import compress from "astro-compress";
 import { defineConfig } from "astro/config";
-import mdx from '@astrojs/mdx';
+import Icons from "unplugin-icons/vite";
+//import vercel from "@astrojs/vercel/serverless"; // Updated import
 
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://salam.app",
-  integrations: [tailwind(), mdx(), solidJs(), compress()]
+  integrations: [tailwind(), sitemap(), solidJs(), compress()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+      noExternal: ["solid-dismiss"],
+    },
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
+  },
+}),
 
-
-});
